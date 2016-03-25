83b23f8afaad55edf2e2852d0d8490d58e3afd35
David Renton <david@rampsoftware.com>
2016-03-25 10:52:56 -0400

    add Redux store

---
+++ voting-server/test/store_spec.js
@@ -0,0 +1,16 @@
+import {Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import makeStore from '../src/store';
+
+describe('store', () => {
+    it('is a Redux store configured with the correct reducer', () => {
+        const store = makeStore();
+        expect(store.getState()).to.equal(Map());
+
+        store.dispatch({type: 'SET_ENTRIES', entries: ['movie1', 'movie2']});
+        expect(store.getState()).to.equal(fromJS({
+            entries: ['movie1', 'movie2']
+        }));
+    });
+});
